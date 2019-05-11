0989969eacb637134aac1e4a4923d84c52d7c9e6
Narayan Prusty <arsenal.narayan@gmail.com>
2019-05-10 19:21:07 -0700

    updated readme and removed requestaccess

--- README.md
+++ README.md
@@ -8,7 +8,8 @@ First ssh into the EC2 that's running the containers. Then access to shell of co
 
 Then follow this steps to install and instantiate the chaincode:
 
-1. We need to install the chaincode in all three authorities containers. For all authorities, clone the chaincode repo using the command `cd /opt/gopath/src/github.com && git clone https://github.com/narayanprusty/mars-identity-chaincode.git`
-2. Then for all authorities install using this command: `peer chaincode install -n identity -v v1.0 -p github.com/mars-identity-chaincode`
-3. Then in identity authority container run the following command to instantiate the chaincode: `peer chaincode instantiate -n identity -v 1.0 -c '{"Args":[]}' -C identity --cafile /home/managedblockchain-tls-chain.pem --tls`
+1. Create the directory using `mkdir /opt/gopath/src && mkdir /opt/gopath/src/github.com` if doesn't exist.
+2. We need to install the chaincode in all three authorities containers. For all authorities, clone the chaincode repo using the command `cd /opt/gopath/src/github.com && git clone https://github.com/narayanprusty/mars-identity-chaincode.git`.
+3. Then for all authorities install using this command: `peer chaincode install -n identity -v 1.0 -p github.com/mars-identity-chaincode`
+4. Then in identity authority container run the following command to instantiate the chaincode: `peer chaincode instantiate -o $ORDERER_URL -C identity -n identity -v 1.0 -c '{"Args":[]}' --cafile /home/crypto/managedblockchain-tls-chain.pem --tls`
 
--- identity_chaincode.go
+++ identity_chaincode.go
@@ -7,9 +7,6 @@ import (
 	pb "github.com/hyperledger/fabric/protos/peer"
 	"github.com/hyperledger/fabric/protos/msp"
 	"github.com/golang/protobuf/proto"
-	"github.com/decred/dcrd/dcrec/secp256k1"
-	"encoding/hex"
-	"crypto/sha256"
 )
 
 type User struct {
@@ -61,14 +58,10 @@ func (t *IdentityChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response
 		return t.issueIdentity(stub, args)
 	} else if function == "getIdentity" {
 		return t.getIdentity(stub, args)
-	} else if function == "updateUserMetadataHash" {
-		return t.updateUserMetadataHash(stub, args)
 	} else if function == "addServiceProvider" {
 		return t.addServiceProvider(stub, args)
 	} else if function == "getServiceProvider" {
 		return t.getServiceProvider(stub, args)
-	} else if function == "requestAccess" {
-		return t.requestAccess(stub, args)
 	}
 
 	return shim.Error("Invalid function name: " + function)
@@ -276,98 +269,6 @@ func (t *IdentityChaincode) getServiceProvider(stub shim.ChaincodeStubInterface,
 	}
 
 	return shim.Success(sp)
-}
-
-func stringInSlice(a string, list []string) bool {
-	for _, b := range list {
-			if b == a {
-					return true
-			}
-	}
-	return false
-}
-
-func (t *IdentityChaincode) requestAccess(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	if len(args) != 2 {
-		return shim.Error("Incorrect number of arguments.")
-	}
-
-	var err error
-
-	user, err := stub.GetState("user_" + args[0])
-
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-
-	var userStruct User
-	err = json.Unmarshal(user, &userStruct)
-
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-
-	identity, err := stub.GetCreator()
-
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-
-	sId := &msp.SerializedIdentity{}
-	err = proto.Unmarshal(identity, sId)
-	
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-
-	nodeId := sId.Mspid
-
-	pubKeyBytes, err := hex.DecodeString(userStruct.PublicKey)
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-
-	pubKey, err := secp256k1.ParsePubKey(pubKeyBytes)
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-
-	sigBytes, err := hex.DecodeString(args[1])
-
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-
-	signature, err := secp256k1.ParseDERSignature(sigBytes)
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-
-	message := []byte("{\"action\":\"grantAccess\",\"to\":\"" + nodeId + "\"}")
-	
-	messageHash := sha256.Sum256([]byte(message))
-	verified := signature.Verify(messageHash[:], pubKey)
-	
-	if (verified) {
-		permissionAdded := stringInSlice(nodeId, userStruct.Permissions)
-
-		if permissionAdded {
-			return shim.Error("Already have permission")
-		}
-
-		userStruct.Permissions = append(userStruct.Permissions, nodeId)
-		userJson, err := json.Marshal(userStruct)
-
-		err = stub.PutState("user_" + args[0], userJson)
-
-		if err != nil {
-			return shim.Error(err.Error())
-		}
-	} else {
-		return shim.Error("Invalid signature")
-	}
-
-	return shim.Success(nil)
 }
 
 func main() {
